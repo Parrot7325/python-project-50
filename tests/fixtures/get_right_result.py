c17746e3023a14a2ccfbd4e769c54f4bae5ea0c3
Parrot7325 <maximus1997@yahoo.com>
2023-02-22 20:08:53 +0300

    Написаны тесты для generate_diff

---
+++ tests/fixtures/get_right_result.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.fixture
+def get_right_result():
+    result = '''{
+  - follow: False
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: True
+}'''
+    return result
