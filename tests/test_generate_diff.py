a72c2b5f17d117c7c587e0be8c736bf84ed3999c
Parrot7325 <maximus1997@yahoo.com>
2023-02-22 21:36:19 +0300

    Тесты обновлены фикстуры

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,9 +1,9 @@
 import json
-from tests.fixtures.get_right_result import get_right_result
 from gendiff.scripts.gendiff import generate_diff
 
 
-def test_generate_diff(get_right_result):
+def test_generate_diff():
+    right_result = open('tests/fixtures/right_result.json').read()
     different = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
     assert type(different) == str
-    assert different == get_right_result
+    assert different+'\n' == right_result
