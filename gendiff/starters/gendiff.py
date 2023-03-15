a19bcb10a30514547a535670227229b8e410f616
Parrot7325 <maximus1997@yahoo.com>
2023-03-15 20:57:13 +0300

    Доделан 5-ый шаг

--- gendiff/starters/gendiff.py
+++ gendiff/starters/gendiff.py
@@ -3,7 +3,11 @@ from gendiff.scripts import gendiff
 
 def main():
     args = gendiff.arguments()
-    print(gendiff.generate_diff(args.first_file, args.second_file))
+    form = args.first_file[len(args.first_file) - 4:]
+    if form == 'json':
+        print(gendiff.generate_diff(args.first_file, args.second_file))
+    elif form == 'yaml' or form == '.yml':
+        print(gendiff.generate_diff_yaml(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
