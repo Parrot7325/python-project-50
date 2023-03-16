1b11b38f2e73c0048de7cba733001ea0bb66bd35
Parrot7325 <maximus1997@yahoo.com>
2023-03-16 22:53:16 +0300

    Добавлены тесты для recursive_generate_diff_yaml

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -23,3 +23,11 @@ def test_recursive_generate_diff():
                                                 'tests/fixtures/recursive_file1.json')
     assert type(different) == str
     assert different + '\n' == right_result
+
+
+def test_recursive_generate_diff_yaml():
+    right_result = open('tests/fixtures/recursive_right_result.yml').read()
+    different = gendiff.recursive_generate_diff_yaml('tests/fixtures/recursive_file1.yml',
+                                                     'tests/fixtures/recursive_file1.json')
+    assert type(different) == str
+    assert different + '\n' == right_result
