ba5fde0f4e367a4a7515e6b5a1376850c1b098f1
Parrot7325 <maximus1997@yahoo.com>
2023-03-24 20:57:59 +0300

    Проходят тесты на рекурсивность

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -20,13 +20,13 @@ def test_generate_diff_yaml():
 def test_recursive_generate_diff():
     right_result = open('tests/fixtures/recursive_right_result.json').read()
     different = gendiff.generate_diff('tests/fixtures/recursive_file1.json', 
-                                      'tests/fixtures/recursive_file1.json')
+                                      'tests/fixtures/recursive_file2.json')
     assert type(different) == str
     assert different + '\n' == right_result
 
 
 def test_recursive_generate_diff_yaml():
-    right_result = open('tests/fixtures/recursive_right_result.yml').read()
+    right_result = open('tests/fixtures/recursive_right_result.json').read()
     different = gendiff.generate_diff_yaml('tests/fixtures/recursive_file1.yml',
                                            'tests/fixtures/recursive_file2.yml')
     assert type(different) == str
