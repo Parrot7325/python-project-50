2459886d09f32918007ecbe9d2b3698f81761017
Parrot7325 <maximus1997@yahoo.com>
2023-03-28 19:31:00 +0300

    Добавлена документация к функциям

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,6 +5,10 @@ from yaml.loader import SafeLoader
 
 
 def arguments():
+    """
+    arguments
+    Принимает и парсит аргументы командной строки.
+    """
     my_parser = argparse.ArgumentParser(prog='gendiff',
                                         description=('Compares two'
                                                      ' configuration '
@@ -19,6 +23,12 @@ def arguments():
 
 
 def stringify_dict(dictionary, depth=1):
+    """
+    stringify_dict(dictionary, depth=1)
+    Принимает словарь и возвращает строку с удобным визуальным
+    отображением исходного словаря.
+    Значения приводятся к стандарту json (None -> null, True -> true и т.д.)
+    """
     if dictionary is None:
         return 'null'
     if type(dictionary) == bool:
@@ -40,46 +50,101 @@ def stringify_dict(dictionary, depth=1):
     return '{\n' + result + f'{(depth - 1) * "    "}' + '}'
 
 
-def gen_base_diff(file1, file2):
+def gen_base_diff(dict1, dict2):
+    """
+    gen_base_diff(dict1, dict2)
+    Принимает два словаря (любой вложенности). Возвращает словарь
+    с описанием различий:
+        unchanged: содержит пары ключ/значение, которые не были изменены
+        removed: содержит пары ключ/значение отсутствующие во 2-ом словаре
+        added: содержит пары ключ/значение, добавленные во 2-ой словарь
+        changed: содержит пары ключ/кортеж или ключ/словарь для измененных 
+            значений. Элементами кортежа будут значения ключа в 1-ом и 2-ом
+            словарях соответственно. Значением ключа будет словарь в случае,
+            если исходные словари по этому ключу сами содержали словари,
+            и те были изменены. Такой словарь будет иметь описаную здесь
+            структуру.
+        keys: список, содержит отсортированый набор неповторяющихся ключей
+            обоих переданых словарей.
+
+    Возвращаемый словарь предполагается для передачи в любой из декораторов.
+    """
     diff = {
         'unchanged': {},
-        'only in first file': {},
-        'only in second file': {},
+        'removed': {},
+        'added': {},
         'changed': {},
         'keys': []
     }
-    keys = list(set(file1.keys()).union(set(file2.keys())))
+    keys = list(set(dict1.keys()).union(set(dict2.keys())))
     diff['keys'].extend(keys)
     for key in keys:
-        if file1.get(key) == file2.get(key):
-            diff['unchanged'][key] = file1.get(key)
+        if dict1.get(key) == dict2.get(key):
+            diff['unchanged'][key] = dict1.get(key)
         else:
-            if key in file1.keys() and key not in file2.keys():
-                diff['only in first file'][key] = file1.get(key)
-            elif key not in file1.keys() and key in file2.keys():
-                diff['only in second file'][key] = file2.get(key)
-            elif key in file1.keys() and key in file2.keys():
-                if type(file1[key]) == dict and type(file2[key]) == dict:
-                    diff['changed'][key] = gen_base_diff(file1[key], file2[key])
+            if key in dict1.keys() and key not in dict2.keys():
+                diff['removed'][key] = dict1.get(key)
+            elif key not in dict1.keys() and key in dict2.keys():
+                diff['added'][key] = dict2.get(key)
+            elif key in dict1.keys() and key in dict2.keys():
+                if type(dict1[key]) == dict and type(dict2[key]) == dict:
+                    diff['changed'][key] = gen_base_diff(dict1[key], dict2[key])
                 else:
-                    diff['changed'][key] = (file1[key], file2[key])
+                    diff['changed'][key] = (dict1[key], dict2[key])
     diff['keys'].sort()
     return diff
 
 
 def gen_text_diff_tree(diff, depth=1):
+    """
+    gen_text_diff_tree(diff, depth=1)
+    Принимает словарь, созданный gen_base_diff. Возвращает строку с
+    представлением изменений в формате дерева. Например:
+        file1.json
+        {
+          "host": "hexlet.io",
+          "timeout": 50,
+          "proxy": "123.234.53.22",
+          "follow": false
+        }
+
+        file2.json
+        {
+          "host": "hexlet.io",
+          "timeout": 50,
+          "proxy": "123.234.53.22",
+          "follow": false
+        }
+
+        gen_text_diff_tree(gen_base_diff(file1.json, file2.json))
+        {
+          - follow: false
+            host: hexlet.io
+          - proxy: 123.234.53.22
+          - timeout: 50
+          + timeout: 20
+          + verbose: true
+        }
+
+        Отсутствие плюса или минуса говорит о том, что ключ есть в обоих
+        файлах, и его значения совпадают. Во всех остальных ситуациях значение
+        по ключу либо отличается, либо ключ есть только в одном файле.
+        В примере выше ключ timeout есть в обоих файлах, но имеет разные
+        значения, proxy находится только в file1.json, а verbose только
+        в file2.json.
+    """
     result = ''
     for key in diff['keys']:
         if key in diff['unchanged'].keys():
             item = stringify_dict(diff["unchanged"][key], depth + 1)
             result += f'{depth * "    "}{key}: {item}\n'
         else:
-            if key in diff['only in first file'].keys():
-                item = stringify_dict(diff["only in first file"][key],
+            if key in diff['removed'].keys():
+                item = stringify_dict(diff["removed"][key],
                                       depth + 1)
                 result += f'{(depth - 1) * "    "}  - {key}: {item}\n'
-            elif key in diff['only in second file'].keys():
-                item = stringify_dict(diff["only in second file"][key],
+            elif key in diff['added'].keys():
+                item = stringify_dict(diff["added"][key],
                                       depth + 1)
                 result += f'{(depth - 1) * "    "}  + {key}: {item}\n'
             elif key in diff['changed'].keys():
@@ -97,6 +162,11 @@ def gen_text_diff_tree(diff, depth=1):
 
 
 def get_value_plain(value):
+    """
+    get_value_plain(value)
+    Возвращает значение, приведенное к формату для представления в
+    формате plain (gen_text_diff_plain).
+    """
     if type(value) == dict:
         return '[complex value]'
     elif type(value) == str:
@@ -106,17 +176,45 @@ def get_value_plain(value):
 
 
 def get_path_plain(previous_path, new_part):
+    """
+    get_path_plain(previous_path, new_part)
+    Принимает путь для обрабатываемого объекта и имя следующего,
+    возвращает путь для конечного описываемого объекта для формата plain
+    (gen_text_diff_plain).
+    """
     new_path = f'{previous_path}.{new_part}'[1:]
     return new_path
 
 
 def gen_text_diff_plain(diff, path=''):
+    """
+    gen_text_diff_plain(diff, path='')
+    Принимает словарь, созданный gen_base_diff. Возвращает строку с
+    представлением изменений в формате plain:
+
+    Property 'common.follow' was added with value: false
+    Property 'common.setting2' was removed
+    Property 'common.setting3' was updated. From true to null
+    Property 'common.setting4' was added with value: 'blah blah'
+    Property 'common.setting5' was added with value: [complex value]
+    Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+    Property 'common.setting6.ops' was added with value: 'vops'
+    Property 'group1.baz' was updated. From 'bas' to 'bars'
+    Property 'group1.nest' was updated. From [complex value] to 'str'
+    Property 'group2' was removed
+    Property 'group3' was added with value: [complex value]
+
+    
+    Если новое значение свойства является составным, то пишется [complex value].
+    Если свойство вложенное, то отображается весь путь до корня, а не только с
+    учетом родителя, например выше это: common.setting6.ops
+    """
     result = ''
     for key in diff['keys']:
-        if key in diff['only in first file'].keys():
+        if key in diff['removed'].keys():
             result += f"Property '{get_path_plain(path, key)}' was removed\n"
-        elif key in diff['only in second file'].keys():
-            item = diff['only in second file'][key]
+        elif key in diff['added'].keys():
+            item = diff['added'][key]
             result += (f"Property '{get_path_plain(path, key)}' was added with"
                        f" value: {get_value_plain(item)}\n")
         elif key in diff['changed'].keys():
@@ -132,6 +230,22 @@ def gen_text_diff_plain(diff, path=''):
 
 
 def gen_text_diff_json(diff):
+    """
+    gen_text_diff_json(diff)
+    Принимает словарь, созданный gen_base_diff. Возвращает строку с
+    представлением изменений в формате json:
+        unchanged: содержит пары ключ/значение, которые не были изменены
+        removed: содержит пары ключ/значение отсутствующие во 2-ом словаре
+        added: содержит пары ключ/значение, добавленные во 2-ой словарь
+        changed: содержит пары ключ/кортеж или ключ/словарь для измененных 
+            значений. Элементами кортежа будут значения ключа в 1-ом и 2-ом
+            словарях соответственно. Значением ключа будет словарь в случае,
+            если исходные словари по этому ключу сами содержали словари,
+            и те были изменены. Такой словарь будет иметь описаную здесь
+            структуру.
+        keys: список, содержит отсортированый набор неповторяющихся ключей
+            обоих переданых словарей.
+    """
     result = json.dumps(diff, sort_keys=True, indent=4)
     return result
 
