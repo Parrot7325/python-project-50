e6f9597213c76d7bba6c60af74e1b581ce57858a
Parrot7325 <maximus1997@yahoo.com>
2023-04-03 14:17:55 +0300

    Добавлена обработка неверно указанного формата

--- gendiff/starters/gendiff.py
+++ gendiff/starters/gendiff.py
@@ -1,14 +1,18 @@
 from gendiff.scripts import gendiff
+from gendiff import formaters
 
 
 def main():
     args = gendiff.arguments()
     if args.output_format == 'plain':
-        decorator = gendiff.gen_text_diff_plain
+        decorator = formaters.gen_text_diff_plain.gen_text_diff_plain
     elif args.output_format == 'json':
-        decorator = gendiff.gen_text_diff_json
+        decorator = formaters.gen_text_diff_json.gen_text_diff_json
+    elif args.output_format == 'stylish' or args.output_format == '':
+        decorator = formaters.stylish
     else:
-        decorator = gendiff.stylish
+        print('Error! Wrong output format')
+        return 'Error! Wrong output format'
     print(gendiff.generate_diff(args.first_file, args.second_file, decorator))
 
 
