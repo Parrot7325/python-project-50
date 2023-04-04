a3ede826da62c587cb3284b5d1cd0dbfe77a63ac
Parrot7325 <maximus1997@yahoo.com>
2023-04-04 12:25:41 +0300

    Тесты переписаны с @pytest.mark.parametrize

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,53 +1,52 @@
+import pytest
 from gendiff.scripts.gendiff import generate_diff
 
 
-def test_generate_diff():
+@pytest.mark.parametrize("file1,file2", [
+    ('tests/fixtures/file1.json', 'tests/fixtures/file2.yml'),
+    ('tests/fixtures/file1.yml', 'tests/fixtures/file2.json')
+])
+def test_generate_diff(file1, file2):
     right_result = open('tests/fixtures/right_result.json').read()
-    different1 = generate_diff('tests/fixtures/file1.json',
-                               'tests/fixtures/file2.yml')
-    different2 = generate_diff('tests/fixtures/file1.yml',
-                               'tests/fixtures/file2.json')
-    assert type(different1) == str
-    assert type(different2) == str
-    assert different1 + '\n' == right_result
-    assert different2 + '\n' == right_result
+    different = generate_diff(file1, file2)
+    assert type(different) == str
+    assert different + '\n' == right_result
 
 
-def test_recursive_generate_diff():
+@pytest.mark.parametrize("file1,file2", [
+    ('tests/fixtures/recursive_file1.json',
+     'tests/fixtures/recursive_file2.yml'),
+    ('tests/fixtures/recursive_file1.yml',
+     'tests/fixtures/recursive_file2.json')
+])
+def test_recursive_generate_diff(file1, file2):
     right_result = open('tests/fixtures/recursive_right_result.json').read()
-    different1 = generate_diff('tests/fixtures/recursive_file1.json',
-                               'tests/fixtures/recursive_file2.yml')
-    different2 = generate_diff('tests/fixtures/recursive_file1.yml',
-                               'tests/fixtures/recursive_file2.json')
-    assert type(different1) == str
-    assert type(different2) == str
-    assert different1 + '\n' == right_result
-    assert different2 + '\n' == right_result
+    different = generate_diff(file1, file2)
+    assert type(different) == str
+    assert different + '\n' == right_result
 
 
-def test_gen_text_diff_plain():
+@pytest.mark.parametrize("file1,file2", [
+    ('tests/fixtures/recursive_file1.json',
+     'tests/fixtures/recursive_file2.yml'),
+    ('tests/fixtures/recursive_file1.yml',
+     'tests/fixtures/recursive_file2.json')
+])
+def test_gen_text_diff_plain(file1, file2):
     right_result = open('tests/fixtures/plain_right_result.json').read()
-    different1 = generate_diff('tests/fixtures/recursive_file1.yml',
-                               'tests/fixtures/recursive_file2.json',
-                               'plain')
-    different2 = generate_diff('tests/fixtures/recursive_file1.json',
-                               'tests/fixtures/recursive_file2.yml',
-                               'plain')
-    assert type(different1) == str
-    assert type(different2) == str
-    assert different1 + '\n' == right_result
-    assert different2 + '\n' == right_result
+    different = generate_diff(file1, file2, 'plain')
+    assert type(different) == str
+    assert different + '\n' == right_result
 
 
-def test_gen_text_diff_json():
+@pytest.mark.parametrize("file1,file2", [
+    ('tests/fixtures/recursive_file1.json',
+     'tests/fixtures/recursive_file2.yml'),
+    ('tests/fixtures/recursive_file1.yml',
+     'tests/fixtures/recursive_file2.json')
+])
+def test_gen_text_diff_json(file1, file2):
     right_result = open('tests/fixtures/json_right_result.json').read()
-    different1 = generate_diff('tests/fixtures/recursive_file1.yml',
-                               'tests/fixtures/recursive_file2.json',
-                               'json')
-    different2 = generate_diff('tests/fixtures/recursive_file1.json',
-                               'tests/fixtures/recursive_file2.yml',
-                               'json')
-    assert type(different1) == str
-    assert type(different2) == str
-    assert different1 + '\n' == right_result
-    assert different2 + '\n' == right_result
+    different = generate_diff(file1, file2, 'json')
+    assert type(different) == str
+    assert different + '\n' == right_result
